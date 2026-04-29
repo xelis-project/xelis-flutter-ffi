af34cc7419f7db53828d51da106b33d052d974e5
Slixe <slixeprivate@gmail.com>
2026-04-29 19:00:53 +0200

    fix example

--- example/integration_test/simple_test.dart
+++ example/integration_test/simple_test.dart
@@ -1,11 +1,33 @@
-import 'package:integration_test/integration_test.dart';
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
-import 'package:xelis_flutter/xelis_flutter.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:xelis_flutter/src/api/network.dart';
+import 'package:xelis_flutter/src/api/precomputed_tables.dart';
+import 'package:xelis_flutter/src/api/wallet.dart' as x_wallet;
+import 'package:xelis_flutter/src/frb_generated.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
   setUpAll(() async => await RustLib.init());
-  test('Can call rust function', () async {
-    expect(greet(name: "Tom"), "Hello, Tom!");
+
+  test('Can create a wallet and retrieve its seed', () async {
+    final dir =
+        '${Directory.systemTemp.path}${Platform.pathSeparator}xelis_test_wallet';
+    final tablesDir =
+        '$dir${Platform.pathSeparator}tables${Platform.pathSeparator}';
+
+    final wallet = await x_wallet.createXelisWallet(
+      name: 'Test Wallet',
+      directory: dir,
+      password: 'password',
+      network: Network.mainnet,
+      precomputedTablesPath: tablesDir,
+      precomputedTableType: const PrecomputedTableType.l1Low(),
+    );
+
+    final seed = await wallet.getSeed();
+    expect(seed, isNotEmpty);
   });
 }
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:xelis_flutter/src/frb_generated.dart';
 import 'package:xelis_flutter/src/api/wallet.dart' as x_wallet;
 import 'package:xelis_flutter/src/api/network.dart';
+import 'package:xelis_flutter/src/api/precomputed_tables.dart';
 
 Future<void> main() async {
   await RustLib.init();
@@ -31,8 +32,10 @@ class _MyAppState extends State<MyApp> {
 
   Future<void> _getSeed() async {
     try {
-      currentDir = "${Directory.current.path}${Platform.pathSeparator}example_data";
-      tableDir = "${currentDir}${Platform.pathSeparator}tables${Platform.pathSeparator}";
+      currentDir =
+          "${Directory.current.path}${Platform.pathSeparator}example_data";
+      tableDir =
+          "${currentDir}${Platform.pathSeparator}tables${Platform.pathSeparator}";
 
       final wallet = await x_wallet.createXelisWallet(
         name: "Test Wallet",
@@ -40,6 +43,7 @@ class _MyAppState extends State<MyApp> {
         password: "password",
         network: Network.mainnet,
         precomputedTablesPath: tableDir!,
+        precomputedTableType: const PrecomputedTableType.l1Low(),
       );
 
       final mnemonic = await wallet.getSeed();
@@ -64,7 +68,8 @@ class _MyAppState extends State<MyApp> {
         body: Center(
           child: loading
               ? const CircularProgressIndicator()
-              : Text('Created Xelis wallet with mnemonic:\n$seed\n\nTable Path: $tableDir'),
+              : Text(
+                  'Created Xelis wallet with mnemonic:\n$seed\n\nTable Path: $tableDir'),
         ),
       ),
     );
